36bc855f622e919370695687bbb9fba7f4e8c496
vibraniumforge <gregm9999@gmail.com>
2019-11-25 16:49:19 -0800

    Done.

--- rendering/app/controllers/responses_controller.rb
+++ rendering/app/controllers/responses_controller.rb
@@ -4,11 +4,11 @@ class ResponsesController < ApplicationController
   # for each of the controller actions,
   # render the data according to the name of the action
   def plain
-    data = "Early bird gets the worm. But cookie taste better than worm. So me sleep in. - Cookie Monster"
+    render plain: data = "Early bird gets the worm. But cookie taste better than worm. So me sleep in. - Cookie Monster"
   end
 
   def html
-    data = "<html>
+    render html: data = "<html>
               <body>
                 <q>Early bird gets the worm. But cookie taste better than worm. So me sleep in.</q>
                 <cite>Cookie Monster</cite>
@@ -17,24 +17,27 @@ class ResponsesController < ApplicationController
   end
 
   def json
-    data = {
+    render json: data = {
       author: "Cookie Monster",
       quote:"Early bird gets the worm. But cookie taste better than worm. So me sleep in.",
     }
   end
 
   def xml
-    data = "<quote>
+    render xml: data = "<quote>
     <author>Cookie Monster</author>
     <text>Early bird gets the worm. But cookie taste better than worm. So me sleep in.</text>
     </quote>"
   end
 
   def csv
-    data =  CSV.generate do |csv|
+    @data =  CSV.generate do |csv|
       csv << ["author", "name"]
       csv << ["Cookie Monster", "Early bird gets the worm. But cookie taste better than worm. So me sleep in."]
     end
+    send_data @data, filename: "users-4.csv"
+
+  
 
     # you'll need to use send_data instead of render
     # if you want the file to have a particular filename, pass the filename: parameter
@@ -42,6 +45,7 @@ class ResponsesController < ApplicationController
 
   def png
     filename = "public/data.png"
+    send_file filename, disposition: :inline
 
     # you'll need to use send_file instead of render
     # if you want the image to show inline, use disposition: :inline
